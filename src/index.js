f2a3a72dba9d2747107feaa2406a6a8a779a334c
OduvanA <145233058+OduvanA@users.noreply.github.com>
2024-08-22 21:59:16 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ console.log(message);
 
 console.log(path.parse('C:\\Users\\ekate\\OneDrive\\Desktop\\index.txt'));
 
-const buffer = await fs.readFile('C:\\Users\\ekate\\OneDrive\\Desktop\\index.txt');
+const buffer = await fs.readFile('C:\\Users\\ekate\\OneDrive\\Desktop\\12-15-23\\index.txt');
 // припустимо, що в файлі hello.txt був текст Hello World!
 
 console.log(buffer);
