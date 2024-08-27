6bed525224ad0e2c6cefd5d793b3ba9e3c9a4b9f
OduvanA <145233058+OduvanA@users.noreply.github.com>
2024-08-26 21:21:12 -0500

    generateContacts

--- src/index.js
+++ src/index.js
@@ -12,3 +12,15 @@ const buffer = await fs.readFile('C:\\Users\\ekate\\OneDrive\\Desktop\\12-15-23\
 
 console.log(buffer);
 ///<Buffer 48 65 6c 6c 6f 20 57 6f 72 6c 64 21>
+
+import { faker } from "@faker-js/faker";
+
+export const createFakeContact = () => ({
+  id: faker.string.uuid(),
+  name: faker.person.fullName(),
+  phone: faker.phone.number(),
+  email: faker.internet.email(),
+  job: faker.person.jobTitle(),
+});
+
+console.log(createFakeContact()); 
--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,5 +1,19 @@
 import { PATH_DB } from '../constants/contacts.js';
+import fs from 'node:fs/promises';
+import DetectFileEncodingAndLanguage from 'detect-file-encoding-and-language';
+import { createFakeContact } from '../utils/createFakeContact.js';
 
-const generateContacts = async (number) => {};
+const generateContacts = async (number) => {
+  const { encoding } = await DetectFileEncodingAndLanguage(PATH_DB);
+  // const newContactList = await fs.readFile(PATH_DB, encoding);
+  const contactList = JSON.parse(await fs.readFile(PATH_DB, encoding));
+  const newContactList = Array(number).fill(0).map(createFakeContact);
+  contactList.push(...newContactList);
+  await fs.writeFile(PATH_DB, JSON.stringify(contactList, null, 2));
+console.log(contactList);
+  // const newContactList = Array(number).fill(0).map(createFakeContact);
+  // const result = await fs.writeFile(PATH_DB, JSON.stringify(newContactList, null, 2));
+  // console.log(result);
+};
 
 generateContacts(5);
